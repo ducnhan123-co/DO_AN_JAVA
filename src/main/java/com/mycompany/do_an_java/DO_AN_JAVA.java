4d825d4755f1e7a1337ee6dbf6d09d0dc7441cdd
Nhan <pducnhan70@gmail.com>
2025-03-04 14:36:02 +0700

    doi qua maven(chua co gi het)

---
+++ src/main/java/com/mycompany/do_an_java/DO_AN_JAVA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.do_an_java;
+
+/**
+ *
+ * @author pducn
+ */
+public class DO_AN_JAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
