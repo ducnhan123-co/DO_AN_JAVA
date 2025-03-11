a3e2f0d608f04b6e2c51995159089504f6faf8fa
LaperCode <lapper2005@gmail.com>
2025-03-11 07:53:15 +0700

    lay

--- src/main/java/com/mycompany/DAO/ConnectionDAL.java
+++ src/main/java/com/mycompany/DAO/ConnectionDAL.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
 public class ConnectionDAL {
     private static final String URL = "jdbc:mysql://localhost:3306/cuahangthucpham";
     private static final String USER = "root"; 
-    private static final String PASSWORD = ""; 
+    private static final String PASSWORD = "123456"; 
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
